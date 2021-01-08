666b7e9078603f53818d0808c1a349444d96efc4
eshell <eshellster@gmail.com>
2021-01-08 22:50:08 +0900

    feat: 스키마 삭제 - 메모리로 대체됨

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: true,
     }),
     RestaurantsModule,
   ],
