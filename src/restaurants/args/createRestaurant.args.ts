521a6f9f71d5b9301b9ca10b6f020a2308415a1e
eshell <eshellster@gmail.com>
2021-01-11 22:16:54 +0900

    유효성 체크

--- src/restaurants/args/createRestaurant.args.ts
+++ src/restaurants/args/createRestaurant.args.ts
@@ -1,13 +1,23 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantArgs {
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field(() => Boolean)
+  @IsBoolean()
   isVeagn: boolean;
+
   @Field(() => String)
+  @IsString()
   address: string;
+
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   ownerName: string;
 }
