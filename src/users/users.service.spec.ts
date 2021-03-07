a5823ba08285751c2c68b6368557f31cae4e3450
eshell <eshellster@gmail.com>
2021-03-07 09:42:27 +0900

    feat: Testing createAccount

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -7,11 +7,13 @@ import { User } from './Entities/user.entity';
 import { Verification } from './Entities/verification.entity';
 import { UsersService } from './users.service';
 
-const mockRepository = {
+// 두곳에 사용되어 두번 작동했다는 오류가 발생
+// 따로 배정해주기 위해 객체를 반환하는 함수로 변경
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 const mockJwtService = {
   sign: jest.fn(),
   verify: jest.fn(),
@@ -26,17 +28,19 @@ type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 describe('UsersService', () => {
   let service: UsersService;
   let usersRepository: MockRepository<User>;
+  let verificationsRepository: MockRepository<Verification>;
+  let mailService: MailService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verification),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -50,27 +54,64 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    mailService = module.get<MailService>(MailService);
     usersRepository = module.get(getRepositoryToken(User));
+    verificationsRepository = module.get(getRepositoryToken(Verification));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
   describe('createAccount', () => {
+    const createAccountArgs = {
+      email: '',
+      password: expect.any(String),
+      role: 0,
+    };
     it('사용자가 존재하는 경우 실패를 반환한다.', async () => {
       usersRepository.findOne.mockResolvedValue({
         id: 1,
         email: '',
+      });
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toMatchObject({
+        ok: false,
+        error: '등록된 사용자 입니다.',
+      });
+    });
+    it('등록 가능한 사용자계정이면 저장한다.', async () => {
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockResolvedValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+      verificationsRepository.create.mockResolvedValue({
+        user: createAccountArgs,
+      });
+      verificationsRepository.save.mockResolvedValue({
+        code: 'code',
       });
       const result = await service.createAccount({
         email: '',
         password: '',
         role: 0,
       });
-      expect(result).toMatchObject({
-        ok: false,
-        error: '등록된 사용자 입니다.',
-      });
+
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(expect.any(Object));
+
+      expect(verificationsRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationsRepository.save).toHaveBeenCalledWith(
+        expect.any(Object),
+      );
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.any(String),
+      );
+      expect(result).toEqual({ ok: true });
     });
   });
 
