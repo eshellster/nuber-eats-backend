5239c4f48d48c8dec2e164f79b9a5ee98aa6635c
eshell <eshellster@gmail.com>
2021-03-09 18:15:38 +0900

    feat: Testing JwtService

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,12 +1,23 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
 
 const TEST_KEY = 'testKey';
+const USER_ID = 1;
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+    verify: jest.fn(() => ({
+      id: USER_ID,
+    })),
+  };
+});
+
 describe('JwtService', () => {
   let service: JwtService;
   beforeEach(async () => {
-    const module = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       providers: [
         JwtService,
         {
@@ -17,6 +28,22 @@ describe('JwtService', () => {
     }).compile();
     service = module.get<JwtService>(JwtService);
   });
-  it.todo('sign');
-  it.todo('verify');
+  describe('sign', () => {
+    it('성공유도: 싸인후 토큰 발행', () => {
+      const userId = USER_ID;
+      const token = service.sign(userId);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: userId }, TEST_KEY);
+    });
+  });
+  describe('verify', () => {
+    it('성공유도: 토큰으로 사용자 아이디 가져오기', () => {
+      const TOKEN = 'TOKEN';
+      const decodedToken = service.verify(TOKEN);
+      expect(decodedToken).toEqual({ id: USER_ID });
+      expect(jwt.verify).toHaveBeenCalledTimes(1);
+      expect(jwt.verify).toHaveBeenCalledWith(TOKEN, TEST_KEY);
+    });
+  });
 });
