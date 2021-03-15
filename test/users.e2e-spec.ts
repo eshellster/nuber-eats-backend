c0662742303a13c0781810fad5d55e262ac02727
eshell <eshellster@gmail.com>
2021-03-15 13:40:31 +0900

    feat: Testing UserMoudle - createAccount

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,12 +19,34 @@ describe('AppController (e2e)', () => {
   });
 
   afterAll(async () => {
+    // 데이터베이스를 드랍시킨다
+    await getConnection().dropDatabase();
+    // 테스트 앱을 종료시킨다
     app.close();
   });
-  it.todo('me');
+  describe('createAccount', () => {
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            createAccount(input:{
+              email:"eshell@nate.com"
+              password:"eshell@nate.com"
+              role:Owner
+            }){
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200);
+    });
+  });
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
