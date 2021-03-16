5978f04f59b4291890bb35fd5d9e8b97304c57f8
eshell <eshellster@gmail.com>
2021-03-16 12:29:51 +0900

    feat: Testing login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,10 +4,21 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
 const GRAPHQL_ENDPOINT = '/graphql';
+const testUser = {
+  email: 'eshell@nate.com',
+  password: 'eshell@nate.com',
+};
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -32,8 +43,8 @@ describe('AppController (e2e)', () => {
           query: `
           mutation{
             createAccount(input:{
-              email:"eshell@nate.com"
-              password:"eshell@nate.com"
+              email:"${testUser.email}"
+              password:"${testUser.password}"
               role:Owner
             }){
               ok
@@ -41,11 +52,127 @@ describe('AppController (e2e)', () => {
             }
           }`,
         })
-        .expect(200);
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${testUser.email}",
+              password:"${testUser.password}",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            '등록된 사용자 입니다.',
+          );
+        });
+    });
+  });
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation{
+          login(input:{
+            email:"${testUser.email}"
+            password:"${testUser.password}"
+          }){
+            ok
+            error
+            token
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+    it('should not be able login with wrong password', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation{
+          login(input:{
+            email:"${testUser.email}"
+            password:""
+          }){
+            ok
+            error
+            token
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('비밀번호가 유효하지 않습니다.');
+        });
+    });
+    it('should not be able login with wrong user', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation{
+          login(input:{
+            email:"eshell@ate.com"
+            password:"eshell@ate.com"
+          }){
+            ok
+            error
+            token
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('사용자가 없습니다.');
+        });
     });
   });
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
