4631028e12bc6431b958c688adf92e786112f654
eshell <eshellster@gmail.com>
2021-03-17 21:27:12 +0900

    fix: user entity email Unique

--- src/users/Entities/user.entity.ts
+++ src/users/Entities/user.entity.ts
@@ -22,7 +22,7 @@ registerEnumType(UserRole, { name: 'UserRole' }); // <-- GraphQL
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
-  @Column()
+  @Column({ unique: true })
   @Field(() => String)
   @IsEmail()
   email: string;
