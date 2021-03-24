7ac69fe6ac24ce5b2009876316583616432e3848
eshell <eshellster@gmail.com>
2021-03-24 21:53:21 +0900

    feat: Entity에 sold out, invisible 추가

--- src/restaurants/entities/dish.entity.ts
+++ src/restaurants/entities/dish.entity.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
-import { IsNumber, IsString, Length } from 'class-validator';
+import { IsBoolean, IsNumber, IsString, Length } from 'class-validator';
 import { CoreEntity } from 'src/common/entites/core.entity';
 import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 import { Restaurant } from './restaurant.entity';
@@ -18,6 +18,16 @@ export class Dish extends CoreEntity {
   @Column()
   @IsNumber()
   price: number;
+
+  @Field(() => Boolean, { defaultValue: false })
+  @Column()
+  @IsBoolean()
+  soldOut: boolean;
+
+  @Field(() => Boolean, { defaultValue: false })
+  @Column()
+  @IsBoolean()
+  invisible: boolean;
 
   @Field(() => String)
   @Column()
