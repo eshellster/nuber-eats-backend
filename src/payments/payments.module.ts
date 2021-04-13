05d766d8698fc219c96c0f98c4532863fe294eea
eshell <eshellster@gmail.com>
2021-04-13 18:14:53 +0900

    feat: createPayment

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PaymentsService } from './payments.service';
-import { PaymentsResolver } from './payments.resolver';
+import { PaymentService } from './payments.service';
+import { PaymentResolver } from './payments.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
-  providers: [PaymentsService, PaymentsResolver],
+  imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
+  providers: [PaymentService, PaymentResolver],
 })
 export class PaymentsModule {}
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,11 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { Repository } from 'typeorm';
+import {
+  CreatePaymentInput,
+  CreatePaymentOutput,
+} from './dtos/create-payment.dto';
 import { Payment } from './entities/payment.entity';
 
 @Injectable()
 export class PaymentService {
   constructor(
     @InjectRepository(Payment) private readonly payments: Repository<Payment>,
+    @InjectRepository(Restaurant)
+    private readonly restaurnats: Repository<Restaurant>,
   ) {}
+
+  async createPayment(
+    owner,
+    { transactionId, restaurantId }: CreatePaymentInput,
+  ): Promise<CreatePaymentOutput> {
+    try {
+      const restaurant = await this.restaurnats.findOne(restaurantId);
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: '레스토랑을 찾을 수 없습니다.',
+        };
+      }
+      if (owner.id !== restaurant.ownerId) {
+        return {
+          ok: false,
+          error: '결재할 권한이 없습니다.',
+        };
+      }
+      await this.payments.save(
+        this.payments.create({ transactionId, user: owner, restaurant }),
+      );
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
 }
